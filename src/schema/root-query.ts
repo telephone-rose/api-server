80182da4adce7f16f55c1e915f6fe17e321276ee
Jordan Van Walleghem <j.vanwall@gmail.com>
2018-06-27 03:45:05 +0200

    Added a method to display the server public ip (#8)

--- src/schema/root-query.ts
+++ src/schema/root-query.ts
@@ -1,13 +1,24 @@
+import axios from "axios";
 import {
   GraphQLNonNull,
   GraphQLObjectType,
   GraphQLObjectTypeConfig,
+  GraphQLString,
 } from "graphql";
 
 import User from "./user";
 
 const config: GraphQLObjectTypeConfig<{}, {}> = {
   fields: () => ({
+    ip: {
+      description: "Only usefull for debuging purpose",
+      resolve: async () => {
+        const result = await axios.get("https://api.ipify.org?format=json");
+
+        return result.data.ip;
+      },
+      type: new GraphQLNonNull(GraphQLString),
+    },
     me: {
       type: new GraphQLNonNull(User),
     },
